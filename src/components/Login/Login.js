54911113a82e26da42359340729b5b03288cefd4
Wruthless <wsgnzls@comcast.net>
2023-01-05 10:27:38 -0500

    Finalized reducer functionality, clean-up.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useReducer} from 'react';
+import React, {useReducer} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -12,52 +12,31 @@ const emailReducer = (state, action) => {
 		};
 	}
 
-	if (action.type === 'INPUT_BLUR') {
-		return {value: state.value, isValid: state.value.includes('@')}
-	}
-
 	return {value: '', isValid: false};
 };
 
 const passwordReducer = (state, action) => {
 	if (action.type === 'USER_INPUT') {
+		console.log("TEST");
 		return {
 			value: action.val,
 			isValid: action.val.trim().length > 6
 		};
 	}
 
-	if (action.type === 'INPUT_BLUR') {
-		return {value: state.value, isValid: state.value.trim().length > 6}
-	}
-
 	return {value: '', isValid: false};
-};
-
-const formReducer = (state, action) => {
-	if (action.type === 'USER_BLUR') {
-	}
 };
 
 const Login = (props) => {
 	const [emailState, dispatchEmail] = useReducer(emailReducer, {value: '', isValid: undefined});
 	const [passwordState, dispatchPassword] = useReducer(passwordReducer, {value: '', isValid: undefined})
-	const [formState, dispatchForm] = useReducer(formReducer, {value:'', isValid: undefined})
-
 
 	const emailChangeHandler = (event) => {
 		dispatchEmail({type: 'USER_INPUT', val: event.target.value});
-		dispatchForm({type: 'USER_INPUT', val: emailState.val})
 	};
 
 	const passwordChangeHandler = (event) => {
 		dispatchPassword({type: 'USER_INPUT', val: event.target.value});
-		dispatchForm({type: 'USER_INPUT', val: passwordState.value});
-
-	};
-
-	const validateFormHandler = (event) => {
-		dispatchForm({type: 'USER_BLUR', emailVal: emailState.value, passVal: passwordState.value});
 	};
 
 	const submitHandler = (event) => {
@@ -99,7 +78,7 @@ const Login = (props) => {
 					/>
 				</div>
 				<div className={classes.actions}>
-					<Button type="submit" className={classes.btn} disabled={passwordState.isValid===false}>
+					<Button type="submit" className={classes.btn} disabled={passwordState.isValid===false || emailState.isValid===false}>
 						Login
 					</Button>
 				</div>
