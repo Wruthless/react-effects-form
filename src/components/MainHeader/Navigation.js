bfd3594a29f5b8656d82ff97fa83967902c440e3
Wruthless <wsgnzls@comcast.net>
2023-01-11 12:57:59 -0500

    Example using react context with the Consumer.

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -7,25 +7,27 @@ const Navigation = (props) => {
 	return (
 		<AuthContext.Consumer>
 			{(ctx) => {
-				<nav className={classes.nav}>
-					<ul>
-						{ctx.isLoggedIn && (
-							<li>
-								<a href="/">Users</a>
-							</li>
-						)}
-						{props.isLoggedIn && (
-							<li>
-								<a href="/">Admin</a>
-							</li>
-						)}
-						{props.isLoggedIn && (
-							<li>
-								<button onClick={props.onLogout}>Logout</button>
-							</li>
-						)}
-					</ul>
-				</nav>
+				return (
+					<nav className={classes.nav}>
+						<ul>
+							{ctx.isLoggedIn && (
+								<li>
+									<a href="/">Users</a>
+								</li>
+							)}
+							{ctx.isLoggedIn && (
+								<li>
+									<a href="/">Admin</a>
+								</li>
+							)}
+							{ctx.isLoggedIn && (
+								<li>
+									<button onClick={props.onLogout}>Logout</button>
+								</li>
+							)}
+						</ul>
+					</nav>
+				)
 			}}
 		</AuthContext.Consumer>
 	);
